679f61382918f82d8b73945e97c43c8841b6df0f
Kamil <02kamil.allishaw@gulllakecs.org>
2020-01-06 05:17:09 -0800

    Intial For Loop Commit

--- ContolFlow.py
+++ ContolFlow.py
@@ -1,58 +1,8 @@
 """
 Programmer: Kamil Allishaw
-Date: 12/16/19
-Program: Guess my Number
+Date: 1/6/20
+Program:
 """
-
-
-MyNumber = 4
-
-print("\nGuess a number between One & Ten.\n")
-
-#ASk users to guess
-
-guess = int(input("Enter a Guess : "))
-
-#Keep asking users to guess number until correct
-
-while guess != MyNumber:
-    if guess == 69:
-        print("\nNice. Try again though.")
-    else:
-        print("\nNot Quite. Try Again!")
-    guess = int(input("\nEnter another Guess : "))
-
-print("\nCorrect! Good job.\n")
-
-
-"""
-Programmer: Kamil Allishaw
-Date: 12/19/19
-Program: One through Ten
-"""
-
-x = 1
-
-#While Loop will see if a condition has been met
-
-while x <= 10:
-    print(x)
-    x+=1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
