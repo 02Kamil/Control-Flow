c7f1238ad6840c6f67955acb9cf88d668a7bca94
Kamil <02kamil.allishaw@gulllakecs.org>
2020-01-15 06:16:53 -0800

    Minor Fix

--- ContolFlow.py
+++ ContolFlow.py
@@ -1,207 +1,44 @@
 
+
 #Programmer: Kamil Allishaw
 #Date: 12/16/19
 #Program: Guess my Number
+
+
 
 MyNumber = 4
 
 print("\nGuess a number between One & Ten.\n")
 
 #ASk users to guess
->>>>>>> Development
 
-This program asks the user for 5 numbers and then sums them
+guess = int(input("Enter a Guess : "))
+
+#Keep asking users to guess number until correct
+
+while guess != MyNumber:
+    if guess == 69:
+        print("\nNice. Try again though.")
+    else:
+        print("\nNot Quite. Try Again!")
+    guess = int(input("\nEnter another Guess : "))
+
+print("\nCorrect! Good job.\n")
+
+
 
 #Programmer: Kamil Allishaw
 #Date: 12/19/19
 #Program: One through Ten
 
+
 x = 1
 
-# While Loop will see if a condition has been met
+#While Loop will see if a condition has been met
 
 while x <= 10:
     print(x)
-    x += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    x+=1
 
 
 
