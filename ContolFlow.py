0f8d28608846fcddee12d3233c0dd10bb1bbe163
Kamil <02kamil.allishaw@gulllakecs.org>
2020-01-23 05:28:16 -0800

    Add double for loop

---
+++ ContolFlow.py
@@ -0,0 +1,9 @@
+
+# Programmer: Kamil Allishaw
+# Date: 1-20-20
+# Program Double for loop
+
+for i in range(3):
+    print("\nHi! I'm outside " + str(i))
+    for j in range(2):
+        print("\n\tHi! I'm inside " + str(j))
