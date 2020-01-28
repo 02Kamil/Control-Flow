0bbc653708e9b4980946eb36dae55fbbded6144e
Kamil <02kamil.allishaw@gulllakecs.org>
2020-01-28 05:40:37 -0800

    While inside for

--- ContolFlow.py
+++ ContolFlow.py
@@ -7,3 +7,16 @@ for i in range(3):
     print("\nHi! I'm outside " + str(i))
     for j in range(2):
         print("\n\tHi! I'm inside " + str(j))
+
+"""
+Programmer: Kamil A
+Date: 1-23-20
+Program: WHile inside a for
+"""
+
+for i in range(4):
+    print("for Loop: " + str(i))
+    kool = i
+    while kool >= 0:
+        print ("\tWhile Loop: " + str(kool))
+        kool = kool - 1
