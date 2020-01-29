449f1b158ef3d43a9b2646b5e4b940bf9e935245
Kamil <02kamil.allishaw@gulllakecs.org>
2020-01-29 05:25:05 -0800

    Reorganization

--- ContolFlow.py
+++ ContolFlow.py
@@ -23,10 +23,18 @@ while guess != MyNumber:
 print("\nCorrect! Good job.\n")
 
 
+# Programmer: Kamil Allishaw
+# Date: 1-20-20
+# Program Double for loop
 
-#Programmer: Kamil Allishaw
-#Date: 12/19/19
-#Program: One through Ten
+for i in range(3):
+    print("\nHi! I'm outside " + str(i))
+    for j in range(2):
+        print("\n\tHi! I'm inside " + str(j))
+
+# Programmer: Kamil Allishaw
+# Date: 12/19/19
+# Program: One through Ten
 
 
 x = 1
@@ -41,7 +49,7 @@ while x <= 10:
 # Date: 1/6/20
 # Program: Running Total. PART II
 
-#This program asks the user for 5 numbers and then sums them
+# This program asks the user for 5 numbers and then sums them
 
 z = 0
 r = int(input("\nHow many numbers would you like to add together?: "))
@@ -55,9 +63,9 @@ print("\nYou sum is: " + str(z))
 print("Use a calculator next time maybe?")
 
 
-#Programmer: Kamil Allishaw
-#Date: 1/6/20
-#Program: Average Test Scores
+# Programmer: Kamil Allishaw
+# Date: 1/6/20
+# Program: Average Test Scores
 
 
 T = 0
@@ -71,17 +79,6 @@ for i in range(n):
 a = T / n
 
 print("\nThe Average across the scores is: " + str(round(a, 3)))
-
-
-
-# Programmer: Kamil Allishaw
-# Date: 1-20-20
-# Program Double for loop
-
-for i in range(3):
-    print("\nHi! I'm outside " + str(i))
-    for j in range(2):
-        print("\n\tHi! I'm inside " + str(j))
 
 """
 Programmer: Kamil A
