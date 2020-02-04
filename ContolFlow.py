2b9dfcd0c2032853bfad2c3cb3b32e56be0ec494
Kamil <02kamil.allishaw@gulllakecs.org>
2020-02-04 05:16:17 -0800

    Add default Parameter values

--- ContolFlow.py
+++ ContolFlow.py
@@ -4,25 +4,36 @@
 name = input("\nwhat is your name?: ")
 x = 15
 
+
 # Greeting Function
 def greeting():
     print("Hello there " + name)
     print("Nice to meetcha!")
     print(x)
 
+
 # Functions and & Local Variables
 def printSomething():
     x = 3
     print(x)
 
+
 # Functions and Parameters
 def PrintN(age):
     print(age)
 
-"""
+
+# Default Parameter values
+def Print2N(x,y = 69):
+    print("First parameter #: " + str(x))
+    print("Second Parameter #: " + str(y))
+
+
+
 greeting()
 printSomething()
 print(x)
-"""
 PrintN(18)
 PrintN(69)
+Print2N(7,3)
+Print2N(32)
