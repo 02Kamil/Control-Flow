6959e12cc5ce555ff827655242b763ca7fd81477
Kamil <02kamil.allishaw@gulllakecs.org>
2020-02-04 05:03:41 -0800

    Add Functions and parameters

--- ContolFlow.py
+++ ContolFlow.py
@@ -1,10 +1,28 @@
 # Date: 2-3-20
-# Greeting function
+# Programmer: Kamil Allishaw
 
 name = input("\nwhat is your name?: ")
+x = 15
 
+# Greeting Function
 def greeting():
     print("Hello there " + name)
     print("Nice to meetcha!")
+    print(x)
 
+# Functions and & Local Variables
+def printSomething():
+    x = 3
+    print(x)
+
+# Functions and Parameters
+def PrintN(age):
+    print(age)
+
+"""
 greeting()
+printSomething()
+print(x)
+"""
+PrintN(18)
+PrintN(69)
